4762f96c75fa37b36cc0240287dec183c1931c61
Zach <zach3lee@gmail.com>
2019-11-11 19:19:58 -0800

    add step to remove readme when copying dotfiles

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,4 +1,6 @@
 dotfiles_repo="https://github.com/abstractlyZach/dotfiles.git" 
 name=$(whoami)
-./put_git_repo.sh "$dotfiles_repo" "/home/$name"
-
+home_dir="/home/$name"
+./put_git_repo.sh "$dotfiles_repo" "$home_dir"
+cd "$home_dir"
+rm README.md
