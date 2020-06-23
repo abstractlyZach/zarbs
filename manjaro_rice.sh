a7c6c26c9214e7feac9c0037603c40cf5ee80109
abstractlyZach <zach3lee@gmail.com>
2020-06-22 23:10:06 -0700

    Add basic manjaro rice

---
+++ manjaro_rice.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# install all the programs in programs.csv
+
+set -x # echo commands
+set -e # exit on failure
+
+git_install() {
+    program_name="$(basename $1 '.git')"
+    target_directory=~/workspace/"$program_name"
+    ./clone_or_pull "$1" "$target_directory" 
+    cd "$target_directory"
+    sudo make clean install
+    cd -
+}
+
+aur_install() { 
+    pacman --noconfirm --needed --sync "$1" 
+}
+
+pip_install() { 
+    pip install "$1" 
+}
+
+tempfile="${HOME}/.cache/programs.csv"
+
+# ignore the first line (headers)
+# https://stackoverflow.com/questions/339483/how-can-i-remove-the-first-line-of-a-text-file-using-bash-sed-script
+tail -n +2 manjaro_programs.csv > "${tempfile}"
+
+while IFS="," read -r tag program comment; do
+    case "$tag" in
+	"G") git_install "$program";;
+	"P") pip_install "$program";;
+	*) aur_install "$program";;
+    esac
+done < "${tempfile}"
